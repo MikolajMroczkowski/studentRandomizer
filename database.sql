ccf9d684e15eafd0ecc5aa8d83b6143a1d2bf558
MikolajMroczkowski <mikolaj.mroczkowski@gmail.com>
2022-03-22 15:34:27 +0100

    new ddl for db

--- database.sql
+++ database.sql
@@ -1,36 +1,50 @@
-create table if not exists studentRandomizer.classes
+create table if not exists users
 (
-    id   int auto_increment
-    primary key,
-    name text null,
-    constraint classes_id_uindex
-    unique (id)
-    );
+    id       int auto_increment
+        primary key,
+    username text not null,
+    password text not null,
+    constraint users_id_uindex
+        unique (id)
+);
 
-create table if not exists studentRandomizer.students
+create table if not exists classes
+(
+    id    int auto_increment
+        primary key,
+    name  text null,
+    owner int  not null,
+    constraint classes_id_uindex
+        unique (id),
+    constraint classes_users_id_fk
+        foreign key (owner) references users (id)
+            on delete cascade
+);
+
+create table if not exists students
 (
     id      int auto_increment
-    primary key,
+        primary key,
     name    text null,
     surname text null,
     class   int  null,
     number  int  null,
     constraint table_name_id_uindex
-    unique (id),
+        unique (id),
     constraint students_classes_id_fk
-    foreign key (class) references studentRandomizer.classes (id)
-    on delete cascade
-    );
+        foreign key (class) references classes (id)
+            on delete cascade
+);
 
-create table if not exists studentRandomizer.probability
+create table if not exists probability
 (
     id      int auto_increment
-    primary key,
+        primary key,
     student int null,
     constraint probability_id_uindex
-    unique (id),
+        unique (id),
     constraint probability_students_id_fk
-    foreign key (student) references studentRandomizer.students (id)
-    on delete cascade
-    );
+        foreign key (student) references students (id)
+            on delete cascade
+);
 
